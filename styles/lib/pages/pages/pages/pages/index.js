30d3084eb271bd9ddb988810181e2072b7ab58a1
urszulaburgott-boop <urszula.burgott@gmail.com>
2025-08-16 18:52:27 +0200

    Create index.js

---
+++ styles/lib/pages/pages/pages/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1 style={{ fontSize: 24, fontWeight: 700 }}>Ahoj z Next.js ✨</h1>
+      <p>Stránka běží. Teď už se build povede.</p>
+    </main>
+  );
+}
