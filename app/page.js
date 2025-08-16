03de494107f9d6db31f18fa7033a2f61c435bb17
urszulaburgott-boop <urszula.burgott@gmail.com>
2025-08-16 19:39:06 +0200

    Delete app/page.js

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <main style={{ padding: 24 }}>
-      <h1 style={{ fontSize: 24, fontWeight: 700 }}>Aplikace běží ✨</h1>
-      <p>Toto je domovská stránka přes <code>app/</code> router.</p>
-    </main>
-  );
-}
