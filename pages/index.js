529f14e2ccebb82edb27150387b5b013727221fd
urszulaburgott-boop <urszula.burgott@gmail.com>
2025-08-16 19:24:05 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Aplikace běží 🎉</h1>
+      <p>Teď můžeme přidat dashboard, chat a další části.</p>
+    </div>
+  );
+}
